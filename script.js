3461ca796967d3be3358bc4ec04571fcf6a450c3
lavacargiovani-dotcom <lavacargiovani@gmail.com>
2026-08-07 21:59:23 -0300

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-const board=document.querySelector('#board');
-const pieces=['♜','♞','♝','♛','♚','♝','♞','♜','♟','♟','♟','♟','♟','♟','♟','♟','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','♙','♙','♙','♙','♙','♙','♙','♙','♖','♘','♗','♕','♔','♗','♘','♖'];
-pieces.forEach((p,i)=>{let s=document.createElement('div');s.className='square '+((Math.floor(i/8)+i)%2?'dark':'light');s.textContent=p;board.appendChild(s)});
